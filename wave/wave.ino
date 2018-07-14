f335e7b2d095df6570b8ed6062d14eee10846c2d
Giselle Serate <giselle.serate@gmail.com>
2018-07-14 16:04:51 -0700

    Make both arms and both eyes do the waving and blinking

--- wave/wave.ino
+++ wave/wave.ino
@@ -1,18 +1,36 @@
 #include <Servo.h>
 
+#define SERVO_L 10
+#define SERVO_R 9
+#define LED_L 8
+#define LED_R 7
+
 Servo servoLeft;          // Define left servo
 Servo servoRight;         // Define right servo
 
 void setup() {
-  servoLeft.attach(10);  // Set left servo to digital pin 10
-  servoRight.attach(9);  // Set right servo to digital pin 9
+  servoLeft.attach(SERVO_L);  // Set left servo to digital pin 10
+  servoRight.attach(SERVO_R);  // Set right servo to digital pin 9
+  pinMode(LED_L, OUTPUT);
+  pinMode(LED_R, OUTPUT);
 }
 
 void loop() {
+  lights(HIGH);
   forward();
-  delay(500);           // Wait 0.2s between back and forth waving. 
+  delay(200);
+  lights(LOW);
   reverse();
-  delay(500);
+  delay(200);
+  lights(HIGH);
+  delay(200);
+  lights(LOW);
+  delay(200);
+}
+
+void lights(int highOrLow) {
+  digitalWrite(LED_L, highOrLow);
+  digitalWrite(LED_R, highOrLow);
 }
 
 void forward() {
