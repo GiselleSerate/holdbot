5918e2d7fd878e2aefec501001063b34ac40e51e
16xchen <xingyao314@gmail.com>
2018-07-15 04:42:08 -0700

    Handler working, mostly integrated

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-from holdbot.hold_handler import run_handler
+from holdbot.hold_handler.hold_handler import run_handler
 
 if __name__ == '__main__':
     run_handler()
--- hold_handler/hold_handler.py
+++ hold_handler/hold_handler.py
@@ -1,6 +1,7 @@
 import threading
 import random
 import serial
+import time
 
 from .proximity import distance
 
@@ -15,7 +16,7 @@ _CLF_CV = threading.Condition()
 class DummyClassifier:
     def classify(self, data):
         rand = random.random()
-        if rand > 0.6:
+        if rand > 0.9:
             return True
         else:
             return False
@@ -25,27 +26,41 @@ def hold_listener():
     global _HOLD_LISTENER_CV
     global _CLF_CV
 
+    #Just for preventing spurious changes
+    change_threshold = 3
+    num_con_changes = 0
     while True:
         cur_state = (distance() < 7.0)
+        time.sleep(0.5)
         if _RUN_HOLD_HANDLER != cur_state:
+            num_con_changes += 1
+            if num_con_changes < change_threshold:
+                continue
+            num_con_changes = 0
             _HOLD_LISTENER_CV.acquire()
             _RUN_HOLD_HANDLER = cur_state
             if not cur_state:
                 _CLF_CV.acquire()
                 _CLF_CV.notifyAll()
                 _CLF_CV.release()
+            print("LISTENER NOTIFIED, RHH IS", _RUN_HOLD_HANDLER)
             _HOLD_LISTENER_CV.notifyAll()
             _HOLD_LISTENER_CV.release()
 
 def classify_input(classifier, data):
     return classifier.classify(data)
 
-def get_input():
+def get_mic_input():
     global _RUN_HOLD_HANDLER
     global _CLF_CV
     global _CLF_QUEUE
+    global _HOLD_LISTENER_CV
 
-    while (_RUN_HOLD_HANDLER):
+    while (True):
+        _HOLD_LISTENER_CV.acquire()
+        while (not _RUN_HOLD_HANDLER):
+            _HOLD_LISTENER_CV.wait()
+        _HOLD_LISTENER_CV.release()
         rand = random.random()
         if rand > 0.6:
             _CLF_CV.acquire()
@@ -56,10 +71,11 @@ def reset_state():
     global _RUN_HOLD_HANDLER
     global _CLF_QUEUE
 
+    print("STATE BEING RESET")
     _RUN_HOLD_HANDLER = False
     _CLF_QUEUE = []
 
-arduinoSerialData = serial.Serial('/dev/ttyS3',9600)
+arduinoSerialData = serial.Serial('/dev/ttyACM0',9600)
 
 def alert_action():
     global _RUN_HOLD_HANDLER
@@ -72,9 +88,9 @@ def alert_action():
         if(oldTime == currTime):
             continue
         if currTime % 10 == 5:
-            arduinoSerialData.write('1')
+            arduinoSerialData.write(b'1')
         elif currTime % 10 == 0:
-            arduinoSerialData.write('0')
+            arduinoSerialData.write(b'0')
 
 def send_alert():
     print("ALERT!")
@@ -86,34 +102,45 @@ def run_handler():
     global _CLF_QUEUE
     global _CLF_CV
 
-    while True:
-        #Run hold activate listener
-        hl_thread = threading.Thread(target=hold_listener)
-        hl_thread.start()
+    classifier = DummyClassifier()
 
+    #Run hold activate listener
+    hl_thread = threading.Thread(target=hold_listener)
+    hl_thread.start()
+    gmi_thread = threading.Thread(target=get_mic_input)
+    gmi_thread.start()
+
+    while True:
         _HOLD_LISTENER_CV.acquire()
         while (not _RUN_HOLD_HANDLER):
+            print("IN MAIN LOOP, HOLD CV")
             _HOLD_LISTENER_CV.wait()
         _HOLD_LISTENER_CV.release()
 
-        #Start listening for new data
-        gi_thread = threading.Thread(target=get_input)
-        gi_thread.start()
+        print("PRE PROCESS")
 
         #Begin classifying incoming data
-        classifier = DummyClassifier()
         while (_RUN_HOLD_HANDLER):
             _CLF_CV.acquire()
             while (not _CLF_QUEUE):
+                print("WAITING TO PROCESS CLF DATA")
                 _CLF_CV.wait()
             if not _RUN_HOLD_HANDLER:
+                print("EXITING CLF PROCESSING")
                 break
+            print("IN INPUT PROCESSING")
             cur_input = _CLF_QUEUE.pop()
-            _RUN_HOLD_HANDLER = classify_input(classifier, cur_input)
+            is_hold = classify_input(classifier, cur_input)
             _CLF_CV.release()
+            if not is_hold:
+                break
 
+        print("EXITING PROCESSING")
         #Assume that now off hold and should alert
         if _RUN_HOLD_HANDLER:
+            print("SENDING ALERT")
             send_alert()
+        else:
+            print("EARLY TERMINATION")
 
         reset_state()
