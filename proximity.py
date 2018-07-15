b653e419587b74de7e41d55844fd2e52dd059b28
16xchen <xingyao314@gmail.com>
2018-07-15 00:49:27 -0700

    dist fct in proximity.py

--- proximity.py
+++ proximity.py
@@ -39,15 +39,19 @@ def distance():
     distance = (TimeElapsed * 34300) / 2
  
     return distance
- 
+
+
+
 if __name__ == '__main__':
-    try:
+    GPIO.cleanup()
+"""    try:
         while True:
             dist = distance()
             print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
+            time.sleep(0.1)
  
         # Reset by pressing CTRL + C
     except KeyboardInterrupt:
         print("Measurement stopped by User")
         GPIO.cleanup()
+"""
