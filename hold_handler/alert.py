d06a115a063e0b340ac641d027ad9fa49247442c
16xchen <xingyao314@gmail.com>
2018-07-15 18:51:40 -0700

    waves now

--- hold_handler/alert.py
+++ hold_handler/alert.py
@@ -5,7 +5,47 @@ import sounddevice as sd
 import soundfile as sf
 
 
-def blink(GPIO_RIGHT_EYE, GPIO_LEFT_EYE, times = 2):
+GPIO.cleanup()
+
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+#set GPIO Pins
+GPIO_LEFT_EYE = 7
+GPIO_RIGHT_EYE= 9
+
+GPIO_LEFT_SPOON = 10
+GPIO_RIGHT_SPOON = 11
+
+ 
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_LEFT_EYE, GPIO.OUT)
+GPIO.setup(GPIO_RIGHT_EYE, GPIO.OUT)
+
+
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_LEFT_SPOON, GPIO.OUT)
+GPIO.setup(GPIO_RIGHT_SPOON, GPIO.OUT)
+
+
+
+def wave(times = 2):
+    L = GPIO.PWM(GPIO_LEFT_SPOON, 50)
+    R = GPIO.PWM(GPIO_RIGHT_SPOON, 50)
+    L.start(7.5)
+    R.start(7.5)
+    for i in range(times):
+        L.ChangeDutyCylce(7.5)
+        R.ChangeDutyCylce(7.5)
+        time.sleep(0.5)
+        L.ChangeDutyCylce(12.5)
+        R.ChangeDutyCylce(12.5)
+        time.sleep(0.5)
+        L.ChangeDutyCylce(2.5)
+        R.ChangeDutyCylce(2.5)
+        time.sleep(0.5)
+
+
+def blink(times = 2):
     for i in range(times):
         GPIO.output(GPIO_RIGHT_EYE, 1)
         GPIO.output(GPIO_LEFT_EYE, 1)
--- hold_handler/run.py
+++ hold_handler/run.py
@@ -2,7 +2,7 @@ from .proximity import distance
 from .classifier import SmartClassifier, record
 from .util import RingBuffer
 from collections import deque 
-from alert import blink, speak
+from .alert import blink, speak
 import RPi.GPIO as GPIO
 import time
 
@@ -10,25 +10,13 @@ import time
 dist_q = deque(maxlen = 10)
 classer = SmartClassifier()
 
-GPIO.cleanup()
-
-#GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
- 
-#set GPIO Pins
-GPIO_LEFT_EYE = 7
-GPIO_RIGHT_EYE= 9
- 
-#set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_LEFT_EYE, GPIO.OUT)
-GPIO.setup(GPIO_RIGHT_EYE, GPIO.OUT)
-
 while True:
     dist_q.append(distance())
     while mean(dist_q) < 6.0:
         rec = record()
         is_hold = classer.classify(rec)
         if not is_hold:
-            blink(GPIO_RIGHT_EYE, GPIO_LEFT_EYE)
+            wave()
+            blink()
             speak()
     time.sleep(0.1)
